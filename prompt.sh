eb3ec4b027ff9e1d364b2d7ddfb1bdf859eb225d
Anderson Abreu <anderson.abreu@datarobot.com>
2022-07-29 16:39:01 -0400

    updates - faster check

--- prompt.sh
+++ prompt.sh
@@ -1,32 +1,39 @@
 find_git_branch() {
   # Based on: http://stackoverflow.com/a/13003854/170413
   local branch
-  if branch=$(git rev-parse --abbrev-ref HEAD 2> /dev/null); then
-    if [[ "$branch" == "HEAD" ]]; then
-      branch='detached*'
-    fi
-    git_branch="($branch)"
+  branch=$(git rev-parse --abbrev-ref HEAD 2> /dev/null)
+  if [ ! -z "$branch" ]; then
+     if [[ "$branch" == "HEAD" ]]; then
+       branch='detached*'
+     fi
+     git_branch="($branch)"
   else
     git_branch=""
   fi
 }
 
 find_git_dirty() {
-  local status=$(git status --porcelain 2> /dev/null)
-  if [[ "$status" != "" ]]; then
-    git_dirty='*'
+  #local status=$(git status --porcelain 2> /dev/null)
+  #if [[ "$status" != "" ]]; then
+  if [ ! -z "$git_branch" ]; then
+    if [[ $(git diff --stat) != '' ]]; then
+      git_dirty='*'
+    else
+      git_dirty=''
+    fi
   else
     git_dirty=''
   fi
 }
 
+##PROMPT_COMMAND="find_git_branch; find_git_dirty; $PROMPT_COMMAND"
 PROMPT_COMMAND="find_git_branch; find_git_dirty; $PROMPT_COMMAND"
-
-# Default Git enabled prompt with dirty state
-# export PS1="\u@\h \w \[$txtcyn\]\$git_branch\[$txtred\]\$git_dirty\[$txtrst\]\$ "
-
-# Another variant:
-# export PS1="\[$bldgrn\]\u@\h\[$txtrst\] \w \[$bldylw\]\$git_branch\[$txtcyn\]\$git_dirty\[$txtrst\]\$ "
-
-# Default Git enabled root prompt (for use with "sudo -s")
-# export SUDO_PS1="\[$bakred\]\u@\h\[$txtrst\] \w\$ "
+#
+## Default Git enabled prompt with dirty state
+## export PS1="\u@\h \w \[$txtcyn\]\$git_branch\[$txtred\]\$git_dirty\[$txtrst\]\$ "
+#
+## Another variant:
+## export PS1="\[$bldgrn\]\u@\h\[$txtrst\] \w \[$bldylw\]\$git_branch\[$txtcyn\]\$git_dirty\[$txtrst\]\$ "
+#
+## Default Git enabled root prompt (for use with "sudo -s")
+## export SUDO_PS1="\[$bakred\]\u@\h\[$txtrst\] \w\$ "
